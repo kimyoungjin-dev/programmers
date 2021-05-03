b4612ae555b7ebf4a0e4c7d1285d6644c803204e
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-05-03 20:52:25 +0900

    Modify

--- 내적.js
+++ 내적.js
@@ -4,10 +4,11 @@ const b = [-3, -1, 0, 2];
 
 const solution = (a, b) => {
   let result = [];
-
+  //a[i]*b[i] 값을 result 배열에 하나하나 저장한다.
   for (let i = 0; i < a.length; i++) {
     result.push(a[i] * b[i]);
   }
+  //result 배열안의 값을 모두더해준다.
   return result.reduce((a, b) => a + b, 0);
 };
 
